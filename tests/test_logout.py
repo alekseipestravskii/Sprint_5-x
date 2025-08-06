03ed868f36c130880640de3afc3338d81372bd29
alekseipestravskii <pan19772012@gmail.com>
2025-08-06 19:07:31 +0400

    Update test_logout.py

--- tests/test_logout.py
+++ tests/test_logout.py
@@ -2,32 +2,34 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 from data import Credentials
-from helper import generate_registration_data
 from locators import Locators
-from curl import *
 
 class TestLogoutFromAccount:
 
-    def test_logout_from_main_page_login_page(self, driver):
-        #arrange
-        self.driver = driver
-        self.driver.get(main_site)
-        # Логинимся в личный кабинет
-        self.driver.find_element(*Locators.ACCOUNT_HEADER_LINK).click()
-        WebDriverWait(self.driver, 5).until(EC.visibility_of_element_located(Locators.LOGIN_BUTTON))
+    def login(self, driver, main_site):
+        """Выполняет вход в личный кабинет."""
+        driver.get(main_site)
+        driver.find_element(*Locators.ACCOUNT_HEADER_LINK).click()
         
-        self.driver.find_element(*Locators.LOGIN_EMAIL).send_keys(Credentials.email)
-        self.driver.find_element(*Locators.LOGIN_PASSWORD).send_keys(Credentials.password)
-        self.driver.find_element(*Locators.LOGIN_BUTTON).click()
-        WebDriverWait(self.driver, 5).until(EC.visibility_of_element_located(Locators.ACCOUNT_HEADER_LINK))
+        WebDriverWait(driver, 5).until(EC.visibility_of_element_located(Locators.LOGIN_BUTTON))
+        driver.find_element(*Locators.LOGIN_EMAIL).send_keys(Credentials.email)
+        driver.find_element(*Locators.LOGIN_PASSWORD).send_keys(Credentials.password)
+        driver.find_element(*Locators.LOGIN_BUTTON).click()
+        
+        # Ждем, пока личный кабинет обновится
+        WebDriverWait(driver, 5).until(EC.visibility_of_element_located(Locators.ACCOUNT_HEADER_LINK))
 
-        #act
+    def test_logout_from_main_page_login_page(self, driver):
+        """Тест: выход из аккаунта на главной странице после входа."""
+        # Arrange
+        main_site = "your_main_site_url_here"
+        self.login(driver, main_site)
 
-        self.driver.find_element(*Locators.ACCOUNT_HEADER_LINK).click()
-        WebDriverWait(self.driver, 5).until(EC.visibility_of_element_located(Locators.ACCOUNT_LOGOUT_BUTTON))
-        self.driver.find_element(*Locators.ACCOUNT_LOGOUT_BUTTON).click()
+        # Act: выход из аккаунта
+        driver.find_element(*Locators.ACCOUNT_HEADER_LINK).click()
+        WebDriverWait(driver, 5).until(EC.visibility_of_element_located(Locators.ACCOUNT_LOGOUT_BUTTON))
+        driver.find_element(*Locators.ACCOUNT_LOGOUT_BUTTON).click()
 
-        WebDriverWait(self.driver, 5).until(EC.visibility_of_element_located(Locators.LOGIN_BUTTON))
-
-        #assert
-        assert self.driver.current_url == account_login
+        # Assert: проверяем, что пользователь вышел
+        WebDriverWait(driver, 5).until(EC.visibility_of_element_located(Locators.LOGIN_BUTTON))
+        assert driver.current_url == "expected_account_login_url_here"
