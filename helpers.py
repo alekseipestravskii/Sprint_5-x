1c3a27a15d788528ea2f5bcbe1e3dc26e2797edb
alekseipestravskii <pan19772012@gmail.com>
2025-08-06 18:39:08 +0400

    Update helpers.py

--- helpers.py
+++ helpers.py
@@ -9,3 +9,16 @@ def wait_and_fill_login_fields(driver):
     driver.find_element(*Locators.LOGIN_EMAIL).send_keys(Credentials.email)
     driver.find_element(*Locators.LOGIN_PASSWORD).send_keys(Credentials.password)
     driver.find_element(*Locators.LOGIN_BUTTON).click()
+
+class RegistrationHelper:
+    def __init__(self, driver):
+        self.driver = driver
+
+    def register_user(self, name, email, password, registration_url):
+        self.driver.get(registration_url)
+        WebDriverWait(self.driver, 5).until(EC.visibility_of_element_located(Locators.REGISTER_BUTTON))
+
+        self.driver.find_element(*Locators.REG_NAME).send_keys(name)
+        self.driver.find_element(*Locators.REG_EMAIL).send_keys(email)
+        self.driver.find_element(*Locators.REG_PASSWORD).send_keys(password)
+        self.driver.find_element(*Locators.REGISTER_BUTTON).click()
