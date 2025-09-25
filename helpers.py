069db522cad79bfe40206a12ecdd65d363a4143e
alekseipestravskii <pan19772012@gmail.com>
2025-09-25 14:43:16 +0400

    Update helpers.py

--- helpers.py
+++ helpers.py
@@ -1,24 +1,13 @@
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from locators import Locators
-from data import Credentials
+import random
+import string
 
-def wait_and_fill_login_fields(driver):
-    WebDriverWait(driver, 5).until(EC.visibility_of_element_located(Locators.LOGIN_BUTTON))
+def generate_registration_data():
+    name_lenght = random.randint(5, 7)
+    name = ''.join(random.choices(string.ascii_lowercase, k=name_lenght) + random.choices(string.digits, k=3))
 
-    driver.find_element(*Locators.LOGIN_EMAIL).send_keys(Credentials.email)
-    driver.find_element(*Locators.LOGIN_PASSWORD).send_keys(Credentials.password)
-    driver.find_element(*Locators.LOGIN_BUTTON).click()
-
-class RegistrationHelper:
-    def __init__(self, driver):
-        self.driver = driver
-
-    def register_user(self, name, email, password, registration_url):
-        self.driver.get(registration_url)
-        WebDriverWait(self.driver, 5).until(EC.visibility_of_element_located(Locators.REGISTER_BUTTON))
-
-        self.driver.find_element(*Locators.REG_NAME).send_keys(name)
-        self.driver.find_element(*Locators.REG_EMAIL).send_keys(email)
-        self.driver.find_element(*Locators.REG_PASSWORD).send_keys(password)
-        self.driver.find_element(*Locators.REGISTER_BUTTON).click()
+    email_length = random.randint(5, 7)
+    email = ''.join(random.choices(string.ascii_lowercase + string.digits, k=email_length)) + '@gmail.com'
+    
+    password_length = random.randint(6, 8)
+    password = ''.join(random.choices(string.ascii_lowercase + string.digits, k=password_length))
+    return name, email, password  # Возвращаем кортеж (name, email, password)
